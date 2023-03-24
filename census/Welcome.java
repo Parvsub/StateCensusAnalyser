5c8af76038b4a4b04c1dd9016e11e4ef94cddf2a
Parvsub <Praveenboda421@gmail.com>
2023-03-24 13:33:26 +0530

    Added: Welcome message is Displayed Updated

--- census/Welcome.java
+++ census/Welcome.java
@@ -2,6 +2,6 @@ package com.census;
 
 public class Welcome {
     public static void main(String[] args) {
-        System.out.println("Weicome to StateCensusAnalyser");
+        System.out.println("Welcome to StateCensusAnalyser");
     }
 
